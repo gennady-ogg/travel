4740561cc95127c53d7badfda06834c08815f907
gennady-ogg <genn.osipov2011@yandex.ru>
2022-01-19 18:10:57 +0300

    travel

---
+++ app/js/main.js
@@ -0,0 +1,5 @@
+(()=>{var __webpack_modules__={"./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/()=>{eval("//-- лоад\nwindow.onload = function () {\n  document.querySelector('.preloader').classList.add(\"preloader-remove\");\n}; //-- навигация\n\n\nvar toggleMenu = document.querySelector('.toggle');\nvar navigation = document.querySelector('.navigation');\n\ntoggleMenu.onclick = function () {\n  toggleMenu.classList.toggle('active');\n  navigation.classList.toggle('active');\n};\n\n//# sourceURL=webpack://coca-cola/./src/js/main.js?")}},__webpack_exports__={};__webpack_modules__["./src/js/main.js"]()})();
+//# sourceMappingURL=main.js.map
